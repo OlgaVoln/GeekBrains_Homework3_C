5c32661b9d7d0d1dcdee83a7eecbdb59437180b7
Olga Volosenko <olgavolosenko@gmail.com>
2023-07-13 20:28:48 +0300

    ДЗ 1

---
+++ 1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+
+class Program
+{
+    static bool IsPalindrome(int number)
+    {
+        string numberString = number.ToString();
+        int length = numberString.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (numberString[i] != numberString[length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < 10000 || number > 99999)
+        {
+            Console.WriteLine("Введено некорректное число. Пожалуйста, введите пятизначное число.");
+            return;
+        }
+
+        if (IsPalindrome(number))
+        {
+            Console.WriteLine("Число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом.");
+        }
+    }
+}
